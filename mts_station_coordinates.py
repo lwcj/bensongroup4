811dcd0e5e3f27c27586ae4b9cde8cbbff5eb8b2
Alexei Demchouk <aleksod@gmail.com>
2017-06-28 16:58:19 -0400

    corrected to using subset2 as we may need all stations that have identical names but are on different lines

--- mts_station_coordinates.py
+++ mts_station_coordinates.py
@@ -10,7 +10,7 @@ subset2 = subset[(subset['GTFS Latitude'] >= 40.731191) &
                      (subset['GTFS Latitude'] <= 40.753512) &
                      (subset['GTFS Longitude'] >= -74.001387) &
                      (subset['GTFS Longitude'] <= -73.977641)]
-subset3 = subset2.drop_duplicates('Stop Name')
+# subset3 = subset2.drop_duplicates('Stop Name')
 
-with open('subset3.pkl', 'wb') as f:
-    pickle.dump(subset3, f)
+with open('subset2.pkl', 'wb') as f:
+    pickle.dump(subset2, f)
