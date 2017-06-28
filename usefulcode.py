b54b6a888404533f71b1b64ba322a9526c93bbcb
jordan leung <jl4700@columbia.edu>
2017-06-28 15:47:58 -0400

    Update usefulcode.py

--- usefulcode.py
+++ usefulcode.py
@@ -1,3 +1,8 @@
+# Libraries
+import pandas as pd
+import datetime as dt
+
+
 turnstiles_df["DATE_TIME"] = pd.to_datetime(turnstiles_df.DATE + " " + turnstiles_df.TIME, format="%m/%d/%Y %H:%M:%S")
 
 turnstiles_df["DAYOFWEEK"] =turnstiles_df["DATE_TIME"].dt.dayofweek
