52a4659355b27a737ed50bb3fb6f526dc48c87ee
jordan leung <jl4700@columbia.edu>
2017-06-28 16:35:41 -0400

    Update jordancode.py

--- jordancode.py
+++ jordancode.py
@@ -1,1 +1,13 @@
 weekdays = turnstiles_df[(turnstiles_df['DAYOFWEEK'] != 5) & (turnstiles_df['DAYOFWEEK'] != 6)]
+
+df = weekdays.groupby(['C/A', 'UNIT', 'SCP', 'STATION', 'DATE_TIME'], as_index = False).ENTRIES.max()
+
+dfshift = df.shift(1)
+
+df['PREVDT'] = dfshift['DATE_TIME']
+df['PREVENT'] = dfshift['ENTRIES']
+
+df = df.iloc[1:]
+
+df['ENTRIESDIFF'] = df['ENTRIES'] - df['PREVENT']
+df['TIMEDIFF'] = df['DATE_TIME'] - df['PREVDT']
