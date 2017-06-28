15ea4edf3cbc18836823a6b4ae9a57e695916bf9
Alexei Demchouk <aleksod@gmail.com>
2017-06-28 16:51:07 -0400

    added station_coordnitates script to extract the list of relevant station names based on geo coordinates

---
+++ mts_station_coordinates.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+df = pd.read_csv('http://web.mta.info/developers/data/nyct/subway/Stations.csv')
+
+subset = df[['Line', 'Stop Name', 'GTFS Latitude', 'GTFS Longitude']]
+subset2 = subset[(subset['GTFS Latitude'] >= 40.731191) &
+                     (subset['GTFS Latitude'] <= 40.753512) &
+                     (subset['GTFS Longitude'] >= -74.001387) &
+                     (subset['GTFS Longitude'] <= -73.977641)]
+subset3 = subset2.drop_duplicates('Stop Name')
+
+with open('subset3.pkl', 'wb') as f:
+    pickle.dump(subset3, f)
